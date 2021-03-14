47f80d22ef10a9f36c36cd031c24cab2353087f0
DESKTOP-PUPD1VH\tunis <ho@tunisofts.com>
2021-03-14 12:59:46 +0100

    page timeline 14/03/2021

--- resources/views/layouts/front/timeline.blade.php
+++ resources/views/layouts/front/timeline.blade.php
@@ -1,11 +1,54 @@
 
-    <section id="initiative" class="about-us-area section-padding">
+    <section id="initiative" class="about-us-area section-padding" style="background:#fff !important;">
         <div class="container">
             <hr>
             <div class="row">
                 <div class="col-md-12">
                     <div class="section-title">
                         <h2>@lang('front.Board of Directors')</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 mx-auto">
+                    <div class="timeline1" style="box-shadow: 0px 6px 0px 0px #0190c1;">
+                        <h3>@lang('front.Chairman of Board of Directors')</h3>
+                        <p>@lang('front.Abdullah bin Maail Al-Qahtani')</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <div class="timelineimg">
+                       <img src="{{ asset('img/timeline/img01.svg') }}" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding-top:1rem">
+                <div class="col-md-4 mx-auto">
+                    <div class="timeline1 imgtime2" style="box-shadow: 0px 6px 0px 0px #24d195;">
+                        <h3>@lang('front.Vice Chairman of the Board of Directors')</h3>
+                        <p>@lang('front.Saad bin Abdullah Al-Qarni')</p>
+                    </div>
+                </div>
+                <div class="col-md-4 mx-auto">
+                    <div class="timeline1" style="box-shadow: 0px 6px 0px 0px #ff8517;">
+                        <h3>@lang('front.Financial supervisor')</h3>
+                        <p>@lang('front.Mohammed bin Abdullah Al-Dossary')</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding-top:1rem">
+                <div class="col-md-4 mx-auto">
+                    <div class="timeline1 imgtime3" style="box-shadow: 0px 6px 0px 0px #00cbe1;">
+                        <h3>@lang('front.Member of the Board of Directors')</h3>
+                        <p>@lang('front.Abdulrahman bin Nasser Al-Maawi')</p>
+                    </div>
+                </div>
+                <div class="col-md-4 mx-auto">
+                    <div class="timeline1" style="box-shadow: 0px 6px 0px 0px #f5b91c;">
+                        <h3>@lang('front.Member of the Board of Directors')</h3>
+                        <p>@lang('front.Hamid bin Saad Al-Ghamdi')</p>
                     </div>
                 </div>
             </div>
