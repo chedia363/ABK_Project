b7a991a3a64257effeacbd40b7a97453b9ad642b
TSP0045\Chedia Hoichtia <chediahoichtia1@gmail.com>
2021-03-18 12:59:54 +0100

    ***dynamic integration front pages (members,tema,investus)

--- app/Http/Controllers/HomefrontController.php
+++ app/Http/Controllers/HomefrontController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Aboutus;
 use App\Policiesprcduralmanuals;
-
-
+use App\Teams;
+use DB;
 class HomefrontController extends Controller
 {
     public function Abakhome()
@@ -20,8 +20,26 @@ class HomefrontController extends Controller
     public function members()
     {
         $aboutuss = Aboutus::first();
-        
-        return view('layouts.front.members', compact('aboutuss'));
+        // get teams records according to design form
+        $teams = DB::table('teams_a_b_k')->limit(3)->get();
+        $teamsfrst =  DB::table('teams_a_b_k')->first();
+        $teamssecnd =  DB::table('teams_a_b_k')->skip(1)->first();
+        $teamsthrd =  DB::table('teams_a_b_k')->skip(2)->first();
+        $teamsfrth =  DB::table('teams_a_b_k')->skip(3)->first();
+        $teamsfve =  DB::table('teams_a_b_k')->skip(4)->first();        
+      
+
+
+        return view('layouts.front.members', 
+        compact(
+            'aboutuss',
+            'teams',
+            'teamsfrst',
+            'teamssecnd',
+            'teamsthrd',
+            'teamsfrth',
+            'teamsfve'
+        ));
     }
 
     public function politics()
@@ -48,6 +66,8 @@ class HomefrontController extends Controller
 
     public function Invest()
     {
-        return view('layouts.front.invest');
+        // get invests records
+        $investus = DB::table('investus_a_b_k')->limit(3)->get();
+        return view('layouts.front.invest', compact('investus'));
     }
 }
